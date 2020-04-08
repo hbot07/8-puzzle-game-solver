3405e756ab3f5f5324040f69ec338e53a07cef21
Parth Thakur <46916990+hbot07@users.noreply.github.com>
2020-04-08 13:33:08 +0530

    Update driver.py

--- driver.py
+++ driver.py
@@ -1,10 +1,3 @@
-"""
-Skeleton code for Project 1 of Columbia University's AI EdX course (8-puzzle).
-Python 3
-"""
-
-import queue as Q
-
 import resource
 
 import sys
@@ -14,12 +7,6 @@ import math
 import time
 
 start_time, end_time = 0.0, 0.0
-
-
-#### SKELETON CODE ####
-
-## The Class that Represents the Puzzle
-
 
 class PuzzleState(object):
     """docstring for PuzzleState"""
@@ -167,12 +154,6 @@ class PuzzleState(object):
 
         return self.children
 
-
-# Function that Writes to output.txt
-
-### Students need to change the method to have the corresponding parameters
-
-# noinspection PyPep8Naming
 def writeOutput(goal, nodes_expanded, max_depth):
     ### Student Code Goes here
     ans = []
@@ -180,15 +161,13 @@ def writeOutput(goal, nodes_expanded, max_depth):
         ans.append(goal.action)
         goal = goal.parent
     end_time = time.time()
-    f = open("output.txt", "w+")
-    f.write("path_to_goal: " + str(list(reversed(ans))) + \
+    print("path_to_goal: " + str(list(reversed(ans))) + \
             "\ncost_of_path: " + str(len(ans)) + \
             "\nnodes_expanded: " + str(nodes_expanded) + \
             "\nsearch_depth: " + str(len(ans)) + \
             "\nmax_search_depth: " + str(max_depth) + \
             "\nrunning_time: " + str(end_time - start_time) + \
             "\nmax_ram_usage: " + str(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss))
-    f.close()
 
 
 def bfs_search(initial_state):
@@ -213,88 +192,26 @@ def bfs_search(initial_state):
                 frontier.append(neighbor)
                 frontier_configs.append(neighbor.config)
                 max_depth = max(max_depth, neighbor.cost)
-
-
-def dfs_search(initial_state):
-    """DFS search"""
-
-    ### STUDENT CODE GOES HERE ###
-    frontier = [initial_state]
-    nodes_expanded = -1
-    frontier_configs = set(initial_state.config)
-    explored = set()
-    max_depth = 0
-    while (len(frontier) != 0):
-        state = frontier.pop(frontier.__len__() - 1)
-        explored.add(state.config)
-        # print(state.display())
-        nodes_expanded += 1
-        print(nodes_expanded)
-        if (test_goal(state)):
-            writeOutput(state, nodes_expanded, max_depth)
-            break
-        for neighbor in reversed(state.expand()):
-            if not (neighbor.config in frontier_configs or neighbor.config in explored):
-                frontier.append(neighbor)
-                frontier_configs.add(neighbor.config)
-                max_depth = max(max_depth, neighbor.cost)
-
-
-def A_star_search(initial_state):
-    """A * search"""
-
-    ### STUDENT CODE GOES HERE ###
-
-
-def calculate_total_cost(state):
-    """calculate the total estimated cost of a state"""
-
-    ### STUDENT CODE GOES HERE ###
-
-
-def calculate_manhattan_dist(idx, value, n):
-    """calculate the manhattan distance of a tile"""
-
-    ### STUDENT CODE GOES HERE ###
-
-
+                
+                
 def test_goal(puzzle_state):
     """test the state is the goal state or not"""
-
-    ### STUDENT CODE GOES HERE ###
     return puzzle_state.config == tuple([0]).__add__(tuple(range(1, puzzle_state.n * puzzle_state.n)))
 
 
 # Main Function that reads in Input and Runs corresponding Algorithm
 
 def main():
+    begin_state = input("Enter board state").split(",")
     start_time = time.time()
-    sm = sys.argv[1].lower()
-
-    begin_state = sys.argv[2].split(",")
 
     begin_state = tuple(map(int, begin_state))
 
     size = int(math.sqrt(len(begin_state)))
 
     hard_state = PuzzleState(begin_state, size)
-
-    if sm == "bfs":
-
-        bfs_search(hard_state)
-
-    elif sm == "dfs":
-
-        dfs_search(hard_state)
-
-    elif sm == "ast":
-
-        A_star_search(hard_state)
-
-    else:
-
-        print("Enter valid command arguments !")
-
+    
+    bfs_search(hard_state)
 
 if __name__ == '__main__':
     main()
